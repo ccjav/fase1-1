3ceae275e04b99b15bc86df49d80aa216886b1da
jja <gitter@tutanota.com>
2017-01-29 20:57:31 -0600

    fin de semana

---
+++ fin_de_semana/conecta_cuatro.rb
@@ -0,0 +1,50 @@
+class ConectFour
+	def initialize#(args)
+		@board = (('(' + " " + ')') * 48).scan(/(.{21})(?=.)/).join("\n")
+		@white = "\u25cf"
+		@black = "\u25cb"
+		# @lastturn = @black
+	end
+
+	def print_board
+		puts (('(' + white + ')') * 48).scan(/(.{21})(?=.)/).join("\n")
+	end
+
+	def drop_chip!(player)
+		puts "columna:"
+		column = gets.chomp.to_i
+		while @board[column] == " "
+			if @board[column + 22] == " "
+				column += 22
+			else
+				@board[column] = player
+				@lastturn = player
+				puts @board
+				last_turn
+			end
+		end
+	end
+
+	def last_turn
+		if @lastturn == @white
+			drop_chip!(@black)
+		elsif @lastturn == @black
+			drop_chip!(@white)
+		else
+			drop_chip!(@white)
+		end
+	end
+
+	def finished?
+
+	end
+
+	def winner
+
+	end
+end
+
+prueba = ConectFour.new
+
+puts prueba.last_turn
+
