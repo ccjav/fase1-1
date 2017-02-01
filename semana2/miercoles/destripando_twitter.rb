6a0fe558367640ca24a438608e58e524e46552a1
phacohen <codea_mac_05@CodeaCamp05s-Mac-mini.local>
2017-02-01 09:42:33 -0600

    twitter

--- semana2/miercoles/destripando_twitter.rb
+++ semana2/miercoles/destripando_twitter.rb
@@ -3,7 +3,7 @@ require 'nokogiri'
 def username
   doc = Nokogiri::HTML(File.open('twitter_account.html'))
   username = doc.search(".ProfileHeaderCard-name > a")
-  puts "Username: #{username.first.inner_text}"
+  puts "\e[#{1}m#{"\e[#{5}m#{"\e[#{7}m#{"\e[#{36}m#{"Username: #{username.first.inner_text}"}"}"}"}\e[0m"
 end
 
 def stats
@@ -16,7 +16,11 @@ def stats
   seguidores = seguidores.first.inner_text
   favoritos = doc.search('[data-nav="favorites"] > .ProfileNav-value')
   favoritos = favoritos.first.inner_text
-  puts "Stats: Tweets: #{tweets}, Siguiendo: #{siguiendo}, Seguidores: #{seguidores}, Favoritos: #{favoritos}"
+  status_bar = "Stats: Tweets: #{tweets}, Siguiendo: #{siguiendo}, Seguidores: #{seguidores}, Favoritos: #{favoritos}"
+  puts "\u2554" + "\u2550" * status_bar.length + "\u2557" 
+  puts "\u2551" + status_bar + "\u2551"
+  puts "\u255A" + "\u2550" * status_bar.length + "\u255D" 
+  
 end
 
 def tweets
@@ -25,9 +29,10 @@ def tweets
   fecha = doc.search('._timestamp')
   retweet = doc.search('[data-modal="ProfileTweet-retweet"] .ProfileTweet-actionCountForPresentation')
   favoritos = doc.search('.js-actionFavorite .ProfileTweet-actionCountForPresentation')
-  puts "Tweets:\n#{fecha[0].inner_text}.: #{texto[0].inner_text}\n\u267B #{retweet[0].inner_text}, \u2665 #{favoritos[0].inner_text}\n\n"
-  puts "#{fecha[1].inner_text}.: #{texto[1].inner_text}\n\u267B #{retweet[2].inner_text}, \u2665 #{favoritos[2].inner_text}\n\n"
-  puts "#{fecha[2].inner_text}.: #{texto[2].inner_text}\n\u267B #{retweet[4].inner_text}, \u2665 #{favoritos[4].inner_text}"
+  puts "\e[#{4}m#{"Tweets:"}\e[0m\n\n"
+  puts "\e[#{47}m#{"#{fecha[0].inner_text}.: #{texto[0].inner_text}"}\e[0m\n" + "\e[#{47}m#{"\e[#{36}m#{"\u267B"} #{retweet[0].inner_text}, \e[#{31}m#{"\u2665"} #{favoritos[0].inner_text}"}\e[0m \n\n"
+  puts "\e[#{47}m#{"#{fecha[1].inner_text}.: #{texto[1].inner_text}"}\e[0m\n" + "\e[#{47}m#{"\e[#{36}m#{"\u267B"} #{retweet[2].inner_text}, \e[#{31}m#{"\u2665"} #{favoritos[2].inner_text}"}\e[0m \n\n"
+  puts "\e[#{47}m#{"#{fecha[2].inner_text}.: #{texto[2].inner_text}"}\e[0m\n" + "\e[#{47}m#{"\e[#{36}m#{"\u267B"} #{retweet[4].inner_text}, \e[#{31}m#{"\u2665"} #{favoritos[4].inner_text}"}\e[0m \n\n"
 end
 
 username
