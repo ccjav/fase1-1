b63f4ecf34889abcf3b2cbc7a5cffff501c99cb8
phacohen <codea_mac_05@CodeaCamp05s-Mac-mini.local>
2017-02-13 08:59:47 -0600

    maraton

--- semana2/viernes/maraton.rb
+++ semana2/viernes/maraton.rb
@@ -1,45 +1,58 @@
 require 'csv'
 
-def hola
-  p "hola, vamos a jugar maratón :O"
-  contador = 0
-  buenas = 0
-  malas = 0
-  array = CSV.read("preguntas.csv")
-  while contador < array.length
-    p "Pregunta: #{array[contador].join(" ")}"
-    contador += 1
-    respuesta = gets.chomp
-    if respuesta == array[contador].join(" ")
-      p "Respuesta: #{respuesta}"
-      p "bien :D"
-      buenas += 1
-    else
-      "Respuesta: #{respuesta}"
-      p "mal :("
-      malas += 1
-    end
-    contador += 2
+class QuestionsParser
+  def to_array
+    @array = CSV.read("preguntas.csv")
   end
-  p "buenas: #{buenas}"
-  p "malas: #{malas}"
 end
 
-hola
+class GameLauncher < QuestionsParser
+  def start_game
+    p "hola, vamos a jugar maratón :D"
+    to_array
+    Questioneer.new.ask_questions
+  end
+end
 
-# p 'hola, vamos a jugar maratón :O'
-# p 'Pregunta: #{preguntas.csv[0]}'
-# respuesta = gets.chomp
-# p "respuesta: #{respuesta}"
-# if respuesta == preguntas.csv[1]
-#   correctas += 1
-#   p "Correcto!"
-# else
-#   incorrectas +=1
-#   p "Incorrecto :("
-# end
-# preguntas.csv[1]+2
+class Questioneer < QuestionsParser
+  def ask_questions
+    contador = 0
+    while contador < to_array.length
+      p "Pregunta: #{@array[contador].join(" ")}"
+      contador += 1
+      respuesta = gets.chomp
+      if respuesta == @array[contador].join(" ")
+        p "Respuesta: #{respuesta}"
+        p "bien :D"
+        AnswerCounter.count_correct(1)
+      else
+        "Respuesta: #{respuesta}"
+        p "mal :("
+        AnswerCounter.count_incorrect(1)
+      end
+      contador += 2
+    end
+    AnswerCounter.results
+  end
+end
 
-# p "correctas : #{correctas}"
-# p "incorrectas: #{incorrectas}"
+class AnswerCounter
+  @buenas = 0
+  @malas = 0
+  def self.count_correct(number)
+    @buenas += number
+  end
 
+  def self.count_incorrect(number)
+    @malas += number
+  end
+
+  def self.results
+    puts "\e[#{5}m#{"Tuviste #{@buenas} respuestas correctas"}\e[0m"
+
+    puts "\e[#{5}m#{"Tuviste #{@malas} respuestas incorrectas"}\e[0m"
+  end
+end
+
+game1 = GameLauncher.new
+game1.start_game
